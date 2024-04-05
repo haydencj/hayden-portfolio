728d44d9bc336113ecfb52b82b0814b7cb1ef938
haydencj <haydenjohns02@gmail.com>
2024-04-05 18:35:51 -0500

    added resume and doc icon

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,11 +9,13 @@ export default function Hero() {
           </p>
           <div className="flex space-x-4">
             <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-              View Projects
+            Projects
             </button>
-            <button className="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-              Download Resume
-            </button>
+            <a href="public/downloads/Hayden-Johnson-Resume.pdf" download className="flex items-center space-x-2 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+            <img src="/icons/document.svg" alt="Resume Icon" className="w-6 h-6" />
+            <span>Resume</span>
+          </a>
+
           </div>
         </div>
       </section>
