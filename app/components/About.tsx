1f75102115600c2d5ae0ef0f5c08fc3f07e059ff
haydencj <haydenjohns02@gmail.com>
2024-04-09 22:14:43 -0500

    added cards and logos to companies

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,40 +1,42 @@
 export default function About() {
-    console.log('Rendering About component');
+  console.log('Rendering About component');
   return (
-    <section className="py-8">
+    <section className="py-6 text-licorice">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div>
-            <h2 className="text-3xl font-bold mb-4">ABOUT</h2>
-            <p className="text-lg mb-4">
+            <h2 className="text-3xl text-snow font-bold mb-4">ABOUT</h2>
+            <p className="text-lg bg-snow rounded-xl p-3 mb-4">
               I have experience working with a wide range of technologies,
-              including React, Node.js, Python, AWS cloud services, and both SQL and NoSQL databases.
-              I enjoy solving complex problems and creating efficient
-              and scalable solutions.
+              including React, Node.js, Python, AWS cloud services, and both SQL
+              and NoSQL databases. I enjoy solving complex problems and creating
+              efficient and scalable solutions.
             </p>
-            <p className="text-lg mb-4">
-              Recently I completed a Software Engineer internship in Tokyo, Japan, where I gained 
-              valuable experience with AWS technologies including DynamoDB, S3, and EC2. Currently, 
-              I'm working as a part-time web developer in Dallas, Texas, while pursuing my studies.
+            <p className="text-lg bg-snow rounded-xl p-3 mb-4">
+              Recently I completed a Software Engineer internship in Tokyo,
+              Japan, where I gained valuable experience with AWS technologies
+              including DynamoDB, S3, and EC2. Currently, I'm working as a
+              part-time web developer in Dallas, Texas, while pursuing my
+              studies.
             </p>
-            <p className="text-lg">
-              Outside of programming, I enjoy reading sci-fi and fantasy novels, gaming, and 
-              spending quality time with friends and my pets.
+            <p className="text-lg bg-snow rounded-xl p-3">
+              Outside of programming, I enjoy reading sci-fi and fantasy novels,
+              gaming, and spending quality time with friends and my pets.
             </p>
           </div>
           <div>
-            <h3 className="text-3xl font-bold mb-4">SKILLS</h3>
-            <ul className="grid grid-cols-4 gap-4">
-              <li>JavaScript</li>
-              <li>React</li>
-              <li>Node.js</li>
-              <li>Python</li>
-              <li>C/C++</li>
-              <li>MySQL</li>
-              <li>MongoDB</li>
-              <li>DynamoDB</li>
-              <li>Git</li>
-              <li>AWS</li>
+            <h3 className="text-3xl text-snow font-bold mb-4">SKILLS</h3>
+            <ul className="grid grid-cols-4 gap-4 ">
+              <li className="bg-snow rounded-xl p-3">JavaScript</li>
+              <li className="bg-snow rounded-xl p-3">React</li>
+              <li className="bg-snow rounded-xl p-3">Node.js</li>
+              <li className="bg-snow rounded-xl p-3">Python</li>
+              <li className="bg-snow rounded-xl p-3">C/C++</li>
+              <li className="bg-snow rounded-xl p-3">MySQL</li>
+              <li className="bg-snow rounded-xl p-3">MongoDB</li>
+              <li className="bg-snow rounded-xl p-3">DynamoDB</li>
+              <li className="bg-snow rounded-xl p-3">Git</li>
+              <li className="bg-snow rounded-xl p-3">AWS</li>
             </ul>
           </div>
         </div>
--- app/components/Experience.tsx
+++ app/components/Experience.tsx
@@ -1,41 +1,90 @@
+import { Link } from '@remix-run/react';
+
 export default function Experience() {
   return (
-    <section className="py-8">
+    <section className="text-licorice py-6">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <div>
-            <h2 className="text-3xl font-bold mb-4">EDU</h2>
-            <div className="mb-4">
-              <p className="text-lg font-bold">
-                The University of Texas at Arlington
-              </p>
-              <p>Bachelor of Science in Computer Science</p>
-              <p>August 2021 - May 2025</p>
+          <div className="w-fit">
+            <h2 className="text-snow text-3xl font-bold mb-4">EDU</h2>
+            <div className="flex items-center bg-snow rounded-xl p-3 mb-4">
+              <Link to="https://www.linkedin.com/school/university-of-texas-at-arlington/">
+                <img
+                  src="/icons/uta_logo.jpeg"
+                  alt="Hayden Johnson"
+                  className="w-16 h-16 rounded-full mr-3 mb-6 sm:mb-0"
+                />
+              </Link>
+              <div>
+                <p className="text-lg font-bold">
+                  The University of Texas at Arlington
+                </p>
+                <p>Bachelor of Science in Computer Science</p>
+                <p>August 2021 - May 2025</p>
+              </div>
             </div>
           </div>
-          <div>
-            <h2 className="text-3xl font-bold mb-4">WORK</h2>
-            <div className="mb-4">
-              <p className="text-lg font-bold">Web Developer</p>
-              <p>Oncor Electric Delivery | Dallas, TX</p>
-              <p>April 2024 - Present</p>
+          <div className="w-fit">
+            <h2 className="text-snow text-3xl font-bold mb-4">WORK</h2>
+            <div className="flex items-center bg-snow rounded-xl p-3 mb-4">
+              <Link to="https://www.linkedin.com/company/oncor-electric-delivery/">
+                <img
+                  src="/icons/oncor_logo.jpeg"
+                  alt="Hayden Johnson"
+                  className="w-16 h-16 rounded-full mr-3 mb-6 sm:mb-0"
+                />
+              </Link>
+              <div>
+                <p className="text-lg font-bold">Web Developer</p>
+                <p>Oncor Electric Delivery | Dallas, TX</p>
+                <p>April 2024 - Present</p>
+              </div>
             </div>
-            <div className="mb-4">
-              <p className="text-lg font-bold">Software Engineer Intern</p>
-              <p>HENNGE Inc. | Tokyo, Japan</p>
-              <p>January 2024 - February 2024</p>
+            <div className="flex items-center bg-snow rounded-xl p-3 mb-4">
+              <Link to="https://www.linkedin.com/company/hennge/">
+                <img
+                  src="/icons/hennge_logo.jpeg"
+                  alt="Hayden Johnson"
+                  className="w-16 h-16 rounded-full mr-3 mb-6 sm:mb-0"
+                />
+              </Link>
+
+              <div>
+                <p className="text-lg font-bold">Software Engineer Intern</p>
+                <p>HENNGE Inc. | Tokyo, Japan</p>
+                <p>January 2024 - February 2024</p>
+              </div>
             </div>
-            <div className="mb-4">
-              <p className="text-lg font-bold">
-                Customer Data Analytics Intern
-              </p>
-              <p>Oncor Electric Delivery | Dallas, TX</p>
-              <p>June 2023 - August 2023</p>
+            <div className="flex items-center bg-snow rounded-xl p-3 mb-4">
+              <Link to="https://www.linkedin.com/company/oncor-electric-delivery/">
+                <img
+                  src="/icons/oncor_logo.jpeg"
+                  alt="Hayden Johnson"
+                  className="w-16 h-16 rounded-full mr-3 mb-6 sm:mb-0"
+                />
+              </Link>
+
+              <div>
+                <p className="text-lg font-bold">
+                  Customer Data Analytics Intern
+                </p>
+                <p>Oncor Electric Delivery | Dallas, TX</p>
+                <p>June 2023 - August 2023</p>
+              </div>
             </div>
-            <div>
-              <p className="text-lg font-bold">AMS Data Analytics Intern</p>
-              <p>Oncor Electric Delivery | Dallas, TX</p>
-              <p>June 2022 - August 2022</p>
+            <div className="flex items-center bg-snow rounded-xl p-3">
+              <Link to="https://www.linkedin.com/company/oncor-electric-delivery/">
+                <img
+                  src="/icons/oncor_logo.jpeg"
+                  alt="Hayden Johnson"
+                  className="w-16 h-16 rounded-full mr-3 mb-6 sm:mb-0"
+                />
+              </Link>
+              <div>
+                <p className="text-lg font-bold">AMS Data Analytics Intern</p>
+                <p>Oncor Electric Delivery | Dallas, TX</p>
+                <p>June 2022 - August 2022</p>
+              </div>
             </div>
           </div>
         </div>
